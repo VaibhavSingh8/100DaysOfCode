4f0c6dd03d740a36d4c5eda1df884df4c12cdab9
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2023-01-24 02:23:07 +0530

    Update script.js

--- calculator-project/js/script.js
+++ calculator-project/js/script.js
@@ -8,11 +8,11 @@ class Calculator{
     allClear(){
         this.previousOperand = '';
         this.currentOperand = '';
-        this.operation = undefined;
+        this.operation = '';
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0,-1);
     }
 
     appendNumber(number){
@@ -20,17 +20,50 @@ class Calculator{
     }
 
     chooseOperation(operation){
+        if(this.currentOperand === '') return;
+        if(this.previousOperand !== ''){
+            this.solve();
+        }
         this.operation = operation;
-        this.previousOperand = this.currentOperand;
+        this.previousOperand = this.currentOperand ;
         this.currentOperand = '' ;
     }
 
     solve(){
+        let result ;
+        const prev = parseInt(this.previousOperand);
+        const current = parseInt(this.currentOperand);
+        if(isNaN(prev) || isNaN(current)) return;
+        switch(this.operation){
+            case '+':
+                result = prev + current ;
+                break;
 
+            case '-':
+                result = prev - current ;
+                break;
+
+            case '*':
+                result = prev * current  ;
+                break;
+
+            case '÷':
+                result = prev / current ;
+                break;
+            
+            default:
+                return;
+        }
+        this.currentOperand = result;
+        this.operation = '';
+        this.previousOperand = '';
     }
 
     updateDisplay(){
         this.currentOperandElement.innerHTML = this.currentOperand;
+        if(this.operation !== null){
+            this.previousOperandElement.innerHTML = `${this.previousOperand} ${this.operation} `;
+        }
     }
 }
 
@@ -56,4 +89,19 @@ operatorButtons.forEach(button => {
         calculator.chooseOperation(button.innerHTML);
         calculator.updateDisplay();
     })
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.solve();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.allClear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
 });
